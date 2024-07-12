8914110bc4cf3bd1932ed88dc9f4692e25a1f3e0
kalinbas <kalinbas@gmail.com>
2024-07-12 12:57:30 -0600

    added base aerodrome

--- src/gauge.ts
+++ src/gauge.ts
@@ -8,7 +8,7 @@ import {
 } from '../generated/templates/CLGauge/CLGauge';
 
 import { CLGauge } from '../generated/Voter/CLGauge'
-import { Incentive, Position, Stake, Unstake, Claim } from '../generated/schema';
+import { Incentive, Position, Stake, Unstake, Claim, Gauge } from '../generated/schema';
 
 
 export function handleDeposit(event: Deposit): void {
@@ -45,44 +45,41 @@ export function handleWithdraw(event: Withdraw): void {
 }
 
 export function handleClaimRewards(event: ClaimRewards): void {
+  let gauge = Gauge.load(event.address.toHex())
+  if (gauge) {
+    gauge.claimTotal = gauge.claimTotal.plus(event.params.amount);
+    gauge.save();
 
-  let contract = CLGauge.bind(event.address)
-  let rewardToken = contract.try_rewardToken()
-  if (rewardToken.reverted) {
-    log.warning("gauge contract call reverted", []);
+    let claim = new Claim(event.transaction.hash.toHex() + "#" + event.logIndex.toHex());
+    claim.txHash = event.transaction.hash;
+    claim.timestamp = event.block.timestamp;
+    claim.blockNumber = event.block.number;
+    claim.amount = event.params.amount;
+    claim.gauge = gauge.id;
+    claim.owner = event.params.from;
+    claim.save();
   }
-
-  let claim = new Claim(event.transaction.hash.toHex() + "#" + event.logIndex.toHex());
-  claim.txHash = event.transaction.hash;
-  claim.timestamp = event.block.timestamp;
-  claim.blockNumber = event.block.number;
-  claim.amount = event.params.amount;
-  claim.rewardToken = rewardToken.value;
-  claim.owner = event.params.from;
-  claim.save();
 }
 
 export function handleNotifyReward(event: NotifyReward): void {
 
-  let contract = CLGauge.bind(event.address)
-  let rewardToken = contract.try_rewardToken()
-  let endTime = contract.try_periodFinish()
-  let rewardRate = contract.try_rewardRate()
-  let pool = contract.try_pool()
+  let gauge = Gauge.load(event.address.toHex())
 
-  if (rewardToken.reverted || endTime.reverted || rewardRate.reverted || pool.reverted) {
-    log.warning("gauge contract call reverted", []);
-  } else {
-    let incentive = new Incentive(event.transaction.hash.toHex() + "#" + event.logIndex.toHex())
-    incentive.reward = rewardRate.value.times(endTime.value.minus(event.block.timestamp))
-    incentive.rewardToken = rewardToken.value
-    incentive.startTime = event.block.timestamp
-    incentive.endTime = endTime.value
-    incentive.gauge = event.address;
-    incentive.pool = pool.value;
-    incentive.save();
+  if (gauge) {
+    let contract = CLGauge.bind(event.address)
+    let endTime = contract.try_periodFinish()
+    let rewardRate = contract.try_rewardRate()
+  
+    if (endTime.reverted || rewardRate.reverted) {
+      log.warning("gauge contract call reverted", []);
+    } else {
+      let incentive = new Incentive(event.transaction.hash.toHex() + "#" + event.logIndex.toHex())
+      incentive.reward = rewardRate.value.times(endTime.value.minus(event.block.timestamp))
+      incentive.startTime = event.block.timestamp
+      incentive.endTime = endTime.value
+      incentive.gauge = gauge.id;
+      incentive.save();
+    }
   }
-
-
 }
 
--- src/voter.ts
+++ src/voter.ts
@@ -5,9 +5,23 @@ import {
 } from '../generated/Voter/Voter';
 
 import { CLGauge as CLGaugeTemplate } from '../generated/templates'
+import { CLGauge } from '../generated/Voter/CLGauge'
+import { Gauge } from '../generated/schema';
+import { BigInt } from '@graphprotocol/graph-ts';
 
 export function handleGaugeCreated(event: GaugeCreated): void {
-  CLGaugeTemplate.create(event.params.gauge)
+  let contract = CLGauge.bind(event.params.gauge);
+  let nft = contract.try_nft();
+  let rewardToken = contract.try_rewardToken();
+  // only works if CLGauge
+  if (!nft.reverted && !rewardToken.reverted) {
+    CLGaugeTemplate.create(event.params.gauge)
+    let gauge = new Gauge(event.params.gauge.toHex())
+    gauge.pool = event.params.pool;
+    gauge.rewardToken = rewardToken.value;
+    gauge.claimTotal = BigInt.fromI32(0);
+    gauge.save();
+  }
 }
 
 export function handleGaugeKilled(event: GaugeKilled): void {
